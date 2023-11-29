8f3fef74fffee8abb29f2edb7eaf477e54b5a685
George Kirby <george.kirby2@nhs.net>
2023-11-29 14:46:55 +0100

    Added dvh tab and views

--- TreatmentPlanReport.cs
+++ TreatmentPlanReport.cs
@@ -9,6 +9,7 @@ using VMS.TPS.Common.Model.API;
 using VMS.TPS.Common.Model.Types;
 using TreatmentPlanReport.Views;
 using TreatmentPlanReport.ViewModels;
+using Prism.Events;
 
 // TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
 [assembly: AssemblyVersion("1.0.0.1")]
@@ -30,15 +31,21 @@ namespace VMS.TPS
         public void Execute(ScriptContext context , System.Windows.Window window, ScriptEnvironment environment)
         {
             // TODO : Add here the code that is called when the script is launched from Eclipse.
+            OxyPlot.Wpf.PlotView dummy = new OxyPlot.Wpf.PlotView();
+            IEventAggregator eventAggregator = new EventAggregator();
+
             window.Content = new MainView
             {
                 DataContext = new MainViewModel
                 (
                     new PatientViewModel(context.Patient),
                     new PlanViewModel(context.PlanSetup),
-                    new FieldViewModel(context.PlanSetup)
+                    new FieldViewModel(context.PlanSetup),
+                    new DVHPlot.ViewModels.DVHViewModel ( context.PlanSetup, eventAggregator),
+                    new DVHPlot.ViewModels.DVHSelectionViewModel ( context.PlanSetup, eventAggregator)
                 )
             };
+
 
         }
     }
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DVHPlot.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,19 @@ namespace TreatmentPlanReport.ViewModels
 {
     public class MainViewModel
     {
-        public MainViewModel(PatientViewModel patientViewModel, PlanViewModel planViewModel, FieldViewModel fieldViewModel)
+        public MainViewModel(PatientViewModel patientViewModel, PlanViewModel planViewModel, FieldViewModel fieldViewModel, DVHViewModel dVHViewModel, DVHSelectionViewModel dVHSelectionViewModel)
         {
             PatientViewModel = patientViewModel;
             PlanViewModel = planViewModel;
             FieldViewModel = fieldViewModel;
+            DVHViewModel = dVHViewModel;
+            DVHSelectionViewModel = dVHSelectionViewModel;
         }
 
         public PatientViewModel PatientViewModel { get; }
         public PlanViewModel PlanViewModel { get; }
         public FieldViewModel FieldViewModel { get; }
+        public DVHViewModel DVHViewModel { get; }
+        public DVHSelectionViewModel DVHSelectionViewModel { get; }
     }
 }
